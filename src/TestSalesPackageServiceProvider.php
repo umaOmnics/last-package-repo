4482df7b3b87796f44ef14f1e2a8b6f41267beab
umaOmnics <yandava@omnics.in>
2025-01-28 16:00:46 +0530

    v1 commit of test-sales-package

--- src/TestSalesPackageServiceProvider.php
+++ src/TestSalesPackageServiceProvider.php
@@ -15,9 +15,9 @@ class TestSalesPackageServiceProvider extends ServiceProvider
     {
         // Load routes, views, migrations, etc.
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/resources/views', 'yourpackage');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'testsales');
         $this->publishes([
-            __DIR__ . '/config/config.php' => config_path('yourpackage.php'),
+            __DIR__ . '/config/config.php' => config_path('testsales.php'),
         ]);
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
 
